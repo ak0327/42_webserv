ee137e586235ea1c57bfbf282b087c56a0c88916
takira <ca.fie.0220@gmail.com>
2023-08-14 14:44:08 +0900

    add: base of unit test

---
+++ test/unit_test/hoge/test_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int	main() {
+
+	std::cout << "this is test" << std::endl;
+	std::cout << " return SUCCESS -> test OK" << std::endl;
+	std::cout << " return FAILURE -> test KO" << std::endl;
+	return 0;
+}
