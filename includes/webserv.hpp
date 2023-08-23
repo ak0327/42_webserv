a6f91556cb734dc46fe69e48842110986e9ee479
takira <ca.fie.0220@gmail.com>
2023-08-24 00:15:22 +0900

    fix(is_valid_config_file_path): change to throw error when argc == 1

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-# define EXECUTABLE_FILE_ONLY_ARGC	1
+#include <string>
+
 # define CONFIG_FILE_GIVEN_ARGC		2
 # define CONFIG_FILE_INDEX			1
 
@@ -17,5 +18,5 @@
 # define INVALID_ARGUMENT_ERROR_MSG	"[Error] invalid argument"
 # define INVALID_PATH_ERROR_MSG		"[Error] invalid file path"
 
-std::string	get_valid_config_file_path(int argc, char **argv);
+std::string get_valid_config_file_path(const char *path);
 bool		is_valid_config_file_path(const char *path);  // todo: static
