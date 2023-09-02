ed8ab1e439cdfc01cc53899dcf0d8364d86a6ba8
molhot <sakata19991214@gmail.com>
2023-09-02 20:20:55 +0900

    コンストラクターの変更

--- srcs/HttpRequest/ValueWeightArraySet.cpp
+++ srcs/HttpRequest/ValueWeightArraySet.cpp
@@ -5,6 +5,24 @@ ValueWeightArraySet::ValueWeightArraySet()
 
 }
 
+ValueWeightArraySet::ValueWeightArraySet()
+{
+
+}
+
+ValueWeightArraySet& ValueWeightArraySet::operator=(const ValueWeightArraySet &other)
+{
+	if (this == &other)
+		return (*this);
+	this->_valueweight_set = other.get_valueweight_set();
+	return (*this);
+}
+
+ValueWeightArraySet::ValueWeightArraySet(const ValueWeightArraySet &other)
+{
+	this->_valueweight_set = other.get_valueweight_set();
+}
+
 ValueWeightArraySet::ValueWeightArraySet(std::map<std::string, double> valueweight_set)
 {
 	this->_valueweight_set = valueweight_set;
