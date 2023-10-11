54c7f03bc2960a55fc2fc7352ebe8c7ca79c0b3a
takira <ca.fie.0220@gmail.com>
2023-10-11 14:15:34 +0900

    add: const field_names

--- srcs/Const/Constant.cpp
+++ srcs/Const/Constant.cpp
@@ -1,13 +1,18 @@
 #include <string>
 #include "Constant.hpp"
 
+////////////////////////////////////////////////////////////////////////////////
+
 const int OK = 0;
 const int NG = 1;
+
+////////////////////////////////////////////////////////////////////////////////
 
 const int STATUS_OK = 200;
 const int STATUS_BAD_REQUEST = 400;
 const int STATUS_SERVER_ERROR = 500;
 
+////////////////////////////////////////////////////////////////////////////////
 
 const char DECIMAL_POINT = '.';
 const char SIGN_PLUS = '+';
@@ -22,16 +27,13 @@ const char CRLF[] = "\r\n";
 
 const char DELIMITERS[] = "\"(),/:;<=>?@[\\]{}";
 
+////////////////////////////////////////////////////////////////////////////////
+
 const char GET_METHOD[] = "GET";
 const char POST_METHOD[] = "POST";
 const char DELETE_METHOD[] = "DELETE";
 
-const char HTTP_1_1[] = "HTTP/1.1";
-const char HTTP_2_0[] = "HTTP/2.0";
-const char HTTP_3_0[] = "HTTP/3.0";
-
 const std::vector<std::string> METHODS = init_methods();
-const std::vector<std::string> HTTP_VERSIONS = init_http_versions();
 
 std::vector<std::string> init_methods() {
 	std::vector<std::string> methods;
@@ -42,6 +44,14 @@ std::vector<std::string> init_methods() {
 	return methods;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+const char HTTP_1_1[] = "HTTP/1.1";
+const char HTTP_2_0[] = "HTTP/2.0";
+const char HTTP_3_0[] = "HTTP/3.0";
+
+const std::vector<std::string> HTTP_VERSIONS = init_http_versions();
+
 std::vector<std::string> init_http_versions() {
 	std::vector<std::string> http_versions;
 
@@ -50,3 +60,91 @@ std::vector<std::string> init_http_versions() {
 	http_versions.push_back(std::string(HTTP_3_0));
 	return http_versions;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+const char ACCEPT[] = "accept";
+const char ACCEPT_CH[] = "accept-ch";
+const char ACCEPT_CHARSET[] = "accept-charset";
+const char ACCEPT_ENCODING[] = "accept-encoding";
+const char ACCEPT_LANGUAGE[] = "accept-language";
+const char ACCEPT_PATCH[] = "accept-patch";
+const char ACCEPT_POST[] = "accept-post";
+const char ACCEPT_RANGES[] = "accept-ranges";
+const char ACCESS_CONTROL_ALLOW_CREDENTIALS[] = "access-control-allow-credentials";
+const char ACCESS_CONTROL_ALLOW_HEADERS[] = "access-control-allow-headers";
+const char ACCESS_CONTROL_ALLOW_METHODS[] = "access-control-allow-methods";
+const char ACCESS_CONTROL_ALLOW_ORIGIN[] = "access-control-allow-origin";
+const char ACCESS_CONTROL_EXPOSE_HEADERS[] = "access-control-expose-headers";
+const char ACCESS_CONTROL_MAX_AGE[] = "access-control-max-age";
+const char ACCESS_CONTROL_REQUEST_HEADERS[] = "access-control-request-headers";
+const char ACCESS_CONTROL_REQUEST_METHOD[] = "access-control-request-method";
+const char AGE[] = "age";
+const char ALLOW[] = "allow";
+const char ALT_SVC[] = "alt-svc";
+const char ALT_USED[] = "alt-used";
+const char AUTHORIZATION[] = "authorization";
+const char CACHE_CONTROL[] = "cache-control";
+const char CLEAR_SITE_DATA[] = "clear-site-data";
+const char CONNECTION[] = "connection";
+const char CONTENT_DISPOSITION[] = "content-disposition";
+const char CONTENT_ENCODING[] = "content-encoding";
+const char CONTENT_LANGUAGE[] = "content-language";
+const char CONTENT_LENGTH[] = "content-length";
+const char CONTENT_LOCATION[] = "content-location";
+const char CONTENT_RANGE[] = "content-range";
+const char CONTENT_SECURITY_POLICY[] = "content-security-policy";
+const char CONTENT_SECURITY_POLICY_REPORT_ONLY[] = "content-security-policy-report-only";
+const char CONTENT_TYPE[] = "content-type";
+const char COOKIE[] = "cookie";
+const char CROSS_ORIGIN_EMBEDDER_POLICY[] = "cross-origin-embedder-policy";
+const char CROSS_ORIGIN_OPENER_POLICY[] = "cross-origin-opener-policy";
+const char CROSS_ORIGIN_RESOURCE_POLICY[] = "cross-origin-resource-policy";
+const char DATE[] = "date";
+const char ETAG[] = "etag";
+const char EXPECT[] = "expect";
+const char EXPECT_CT[] = "expect-ct";
+const char EXPIRES[] = "expires";
+const char FORWARDED[] = "forwarded";
+const char FROM[] = "from";
+const char HOST[] = "host";
+const char IF_MATCH[] = "if-match";
+const char IF_MODIFIED_SINCE[] = "if-modified-since";
+const char IF_NONE_MATCH[] = "if-none-match";
+const char IF_RANGE[] = "if-range";
+const char IF_UNMODIFIED_SINCE[] = "if-unmodified-since";
+const char KEEP_ALIVE[] = "keep-alive";
+const char LAST_MODIFIED[] = "last-modified";
+const char LINK[] = "link";
+const char LOCATION[] = "location";
+const char MAX_FORWARDS[] = "max-forwards";
+const char ORIGIN[] = "origin";
+const char PERMISSION_POLICY[] = "permission-policy";
+const char PROXY_AUTHENTICATE[] = "proxy-authenticate";
+const char PROXY_AUTHORIZATION[] = "proxy-authorization";
+const char RANGE[] = "range";
+const char REFERER[] = "referer";
+const char RETRY_AFTER[] = "retry-after";
+const char SEC_FETCH_DEST[] = "sec-fetch-dest";
+const char SEC_FETCH_MODE[] = "sec-fetch-mode";
+const char SEC_FETCH_SITE[] = "sec-fetch-site";
+const char SEC_FETCH_USER[] = "sec-fetch-user";
+const char SEC_PURPOSE[] = "sec-purpose";
+const char SEC_WEBSOCKET_ACCEPT[] = "sec-websocket-accept";
+const char SERVER[] = "server";
+const char SERVER_TIMING[] = "server-timing";
+const char SERVICE_WORKER_NAVIGATION_PRELOAD[] = "service-worker-navigation-preload";
+const char SET_COOKIE[] = "set-cookie";
+const char SOURCEMAP[] = "sourcemap";
+const char STRICT_TRANSPORT_SECURITY[] = "strict-transport-security";
+const char TE[] = "te";
+const char TIMING_ALLOW_ORIGIN[] = "timing-allow-origin";
+const char TRAILER[] = "trailer";
+const char TRANSFER_ENCODING[] = "transfer-encoding";
+const char UPGRADE[] = "upgrade";
+const char UPGRADE_INSECURE_REQUESTS[] = "upgrade-insecure-requests";
+const char USER_AGENT[] = "user-agent";
+const char VARY[] = "vary";
+const char VIA[] = "via";
+const char WWW_AUTHENTICATE[] = "www-authenticate";
+const char X_CUSTOM_HEADER[] = "x-custom-header";
--- srcs/Const/Constant.hpp
+++ srcs/Const/Constant.hpp
@@ -3,12 +3,18 @@
 # include <string>
 # include <vector>
 
+////////////////////////////////////////////////////////////////////////////////
+
 extern const int OK;
 extern const int NG;
+
+////////////////////////////////////////////////////////////////////////////////
 
 extern const int STATUS_OK;
 extern const int STATUS_BAD_REQUEST;
 extern const int STATUS_SERVER_ERROR;
+
+////////////////////////////////////////////////////////////////////////////////
 
 extern const char DECIMAL_POINT;
 extern const char SIGN_PLUS;
@@ -23,16 +29,110 @@ extern const char CRLF[];
 
 extern const char DELIMITERS[];
 
+////////////////////////////////////////////////////////////////////////////////
+
 extern const char GET_METHOD[];
 extern const char POST_METHOD[];
 extern const char DELETE_METHOD[];
+
+extern const std::vector<std::string> METHODS;
+
+std::vector<std::string> init_methods();
+
+////////////////////////////////////////////////////////////////////////////////
 
 extern const char HTTP_1_1[];
 extern const char HTTP_2_0[];
 extern const char HTTP_3_0[];
 
-extern const std::vector<std::string> METHODS;
 extern const std::vector<std::string> HTTP_VERSIONS;
 
-std::vector<std::string> init_methods();
 std::vector<std::string> init_http_versions();
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern const char ACCEPT[];
+extern const char ACCEPT_CH[];
+extern const char ACCEPT_CHARSET[];
+extern const char ACCEPT_ENCODING[];
+extern const char ACCEPT_LANGUAGE[];
+extern const char ACCEPT_PATCH[];
+extern const char ACCEPT_POST[];
+extern const char ACCEPT_RANGES[];
+extern const char ACCESS_CONTROL_ALLOW_CREDENTIALS[];
+extern const char ACCESS_CONTROL_ALLOW_HEADERS[];
+extern const char ACCESS_CONTROL_ALLOW_METHODS[];
+extern const char ACCESS_CONTROL_ALLOW_ORIGIN[];
+extern const char ACCESS_CONTROL_EXPOSE_HEADERS[];
+extern const char ACCESS_CONTROL_MAX_AGE[];
+extern const char ACCESS_CONTROL_REQUEST_HEADERS[];
+extern const char ACCESS_CONTROL_REQUEST_METHOD[];
+extern const char AGE[];
+extern const char ALLOW[];
+extern const char ALT_SVC[];
+extern const char ALT_USED[];
+extern const char AUTHORIZATION[];
+extern const char CACHE_CONTROL[];
+extern const char CLEAR_SITE_DATA[];
+extern const char CONNECTION[];
+extern const char CONTENT_DISPOSITION[];
+extern const char CONTENT_ENCODING[];
+extern const char CONTENT_LANGUAGE[];
+extern const char CONTENT_LENGTH[];
+extern const char CONTENT_LOCATION[];
+extern const char CONTENT_RANGE[];
+extern const char CONTENT_SECURITY_POLICY[];
+extern const char CONTENT_SECURITY_POLICY_REPORT_ONLY[];
+extern const char CONTENT_TYPE[];
+extern const char COOKIE[];
+extern const char CROSS_ORIGIN_EMBEDDER_POLICY[];
+extern const char CROSS_ORIGIN_OPENER_POLICY[];
+extern const char CROSS_ORIGIN_RESOURCE_POLICY[];
+extern const char DATE[];
+extern const char ETAG[];
+extern const char EXPECT[];
+extern const char EXPECT_CT[];
+extern const char EXPIRES[];
+extern const char FORWARDED[];
+extern const char FROM[];
+extern const char HOST[];
+extern const char IF_MATCH[];
+extern const char IF_MODIFIED_SINCE[];
+extern const char IF_NONE_MATCH[];
+extern const char IF_RANGE[];
+extern const char IF_UNMODIFIED_SINCE[];
+extern const char KEEP_ALIVE[];
+extern const char LAST_MODIFIED[];
+extern const char LINK[];
+extern const char LOCATION[];
+extern const char MAX_FORWARDS[];
+extern const char ORIGIN[];
+extern const char PERMISSION_POLICY[];
+extern const char PROXY_AUTHENTICATE[];
+extern const char PROXY_AUTHORIZATION[];
+extern const char RANGE[];
+extern const char REFERER[];
+extern const char RETRY_AFTER[];
+extern const char SEC_FETCH_DEST[];
+extern const char SEC_FETCH_MODE[];
+extern const char SEC_FETCH_SITE[];
+extern const char SEC_FETCH_USER[];
+extern const char SEC_PURPOSE[];
+extern const char SEC_WEBSOCKET_ACCEPT[];
+extern const char SERVER[];
+extern const char SERVER_TIMING[];
+extern const char SERVICE_WORKER_NAVIGATION_PRELOAD[];
+extern const char SET_COOKIE[];
+extern const char SOURCEMAP[];
+extern const char STRICT_TRANSPORT_SECURITY[];
+extern const char TE[];
+extern const char TIMING_ALLOW_ORIGIN[];
+extern const char TRAILER[];
+extern const char TRANSFER_ENCODING[];
+extern const char UPGRADE[];
+extern const char UPGRADE_INSECURE_REQUESTS[];
+extern const char USER_AGENT[];
+extern const char VARY[];
+extern const char VIA[];
+extern const char WWW_AUTHENTICATE[];
+extern const char X_CUSTOM_HEADER[];
