d6e381547aa04cba8b04d5ce7f8d76d32c8f3a6e
takira <ca.fie.0220@gmail.com>
2023-11-07 11:38:17 +0900

    refactor

--- srcs/HttpResponse/GET/get_cgi_result.cpp
+++ srcs/HttpResponse/GET/get_cgi_result.cpp
@@ -30,6 +30,7 @@ const int CLOSE_ERROR = -1;
 const int DUP_ERROR = -1;
 const int FORK_ERROR = -1;
 const int SOCKETPAIR_ERROR = -1;
+const ssize_t RECV_ERROR = -1;
 
 const int FLAG_NONE = 0;
 const int CHILD_PROC = 0;
@@ -68,7 +69,7 @@ int execute_cgi_script_in_child(int socket_fds[2],
 							 const std::string &query) {
 	Result<std::vector<std::string>, int> interpreter_result;
 	std::vector<std::string> interpreter;
-	std::vector<char *>argv;
+	std::vector<char *> argv;
 	std::string err_info;
 	(void)query;
 
@@ -117,12 +118,13 @@ bool is_exec_timeout(time_t start_time, int timeout_sec) {
 
 }  // namespace
 
+
 Result<std::vector<std::string>, int> get_interpreter(const std::string &file_path) {
 	std::vector<std::string> interpreter;
 	std::ifstream		file;
 	std::string			shebang_line;
 	std::string			word;
-	const std::size_t	shebang_len = 2;
+	const std::size_t	kSHEBANG_LEN = 2;
 
 	file.open(file_path.c_str());
 	if (file.fail()) {
@@ -144,19 +146,20 @@ Result<std::vector<std::string>, int> get_interpreter(const std::string &file_pa
 		return Result<std::vector<std::string>, int>::err(ERR);
 	}
 
-	if (shebang_len <= (*itr).length() && (*itr)[0] == '#' && (*itr)[1] == '!') {
-		*itr = (*itr).substr(shebang_len);
+	if (kSHEBANG_LEN <= (*itr).length() && (*itr)[0] == '#' && (*itr)[1] == '!') {
+		*itr = (*itr).substr(kSHEBANG_LEN);
 		return Result<std::vector<std::string>, int>::ok(interpreter);
 	}
 	return Result<std::vector<std::string>, int>::err(ERR);
 }
 
-// tmp
 Result<int, int> wait_for_io_ready(int fd) {
-	IOMultiplexer *fds;
 	Result<int, std::string> fd_ready_result;
-	std::string err_info;
-	int ready_fd;
+	IOMultiplexer	*fds = NULL;
+	std::string		err_info;
+	int				ready_fd;
+	bool			is_error = false;
+	bool 			is_timeout = false;
 
 	try {
 #if defined(__linux__) && !defined(USE_SELECT_MULTIPLEXER)
@@ -171,19 +174,26 @@ Result<int, int> wait_for_io_ready(int fd) {
 			fd_ready_result = fds->get_io_ready_fd();
 			if (fd_ready_result.is_err()) {
 				err_info = fd_ready_result.get_err_value();
-				std::cerr << err_info << std::endl;  // todo: tmp
-				delete fds;
-				return Result<int, int>::err(ERR);
+				is_error |= true;
+				break;
 			}
 			if (fd_ready_result.get_ok_value() == IO_TIMEOUT) {
-				std::cerr << "[CGI Error] timeout" << std::endl;
-				delete fds;
-				return Result<int, int>::ok(IO_TIMEOUT);
+				is_timeout |= true;
+				break;
 			}
 			ready_fd = fd_ready_result.get_ok_value();
 			break;
 		}
+
 		delete fds;
+		if (is_error) {
+			std::cerr << err_info << std::endl;  // todo: tmp
+			return Result<int, int>::err(ERR);
+		}
+		if (is_timeout) {
+			std::cerr << "[CGI Error] timeout" << std::endl;  // todo: tmp
+			return Result<int, int>::ok(IO_TIMEOUT);
+		}
 		return Result<int, int>::ok(ready_fd);
 	} catch (std::bad_alloc const &e) {
 		err_info = create_error_info("Failed to allocate memory", __FILE__, __LINE__);
@@ -192,20 +202,78 @@ Result<int, int> wait_for_io_ready(int fd) {
 	}
 }
 
-Result<std::string, int> get_cgi_result_via_socket(int socket_fds[2], int pid) {
+Result<int, int> wait_child_process(pid_t child_pid) {
+	int			child_status;
+	pid_t		wait_result;
+	std::string err_info;
+
+	errno = 0;
+	child_status = EXIT_SUCCESS;
+	wait_result = waitpid(child_pid, &child_status, WNOHANG);
+	// std::cout << CYAN << "errno:" << errno << ", ECHILD:" << ECHILD << RESET << std::endl;
+	if (wait_result == STATUS_SERVER_ERROR && errno != ECHILD) {
+		errno = 0;
+		if (kill(child_pid, SIGKILL) == STATUS_SERVER_ERROR) {
+			err_info = create_error_info(errno, __FILE__, __LINE__);
+			std::cerr << err_info << std::endl;  // todo: tmp
+			return Result<int, int>::err(ERR);
+		}
+	}
+	return Result<int, int>::ok(WEXITSTATUS(child_status));
+}
+
+Result<std::string, int> recv_cgi_result(int read_fd, bool *is_error, bool *is_timeout) {
+	time_t		start_time;
 	std::string	cgi_result;
+	int			kTIMEOUT_SEC = 2;
+	ssize_t 	read_bytes;
 	std::string err_info;
 	char		buf[BUFSIZ];
-	ssize_t 	read_bytes;
+
+	start_time = time(NULL);
+	cgi_result = "";
+	while (true) {
+		if (*is_error || *is_timeout) {
+			break;
+		}
+
+		if (is_exec_timeout(start_time, kTIMEOUT_SEC)) {
+			*is_timeout |= true;
+			break;
+		}
+
+		errno = 0;
+		read_bytes = recv(read_fd, buf, BUFSIZ - 1, FLAG_NONE);
+		if (read_bytes == RECV_ERROR) {
+			err_info = create_error_info(errno, __FILE__, __LINE__);
+			std::cerr << err_info << std::endl;  // todo: tmp
+			*is_error |= true;
+			break;
+		}
+		if (read_bytes == 0) {
+			break;
+		}
+		buf[read_bytes] = '\0';
+		cgi_result.append(buf);
+	}
+
+	if (*is_timeout) {
+		return Result<std::string, int>::err(ERR);
+	}
+	if (*is_error) {
+		return Result<std::string, int>::err(ERR);
+	}
+	return Result<std::string, int>::ok(cgi_result);
+}
+
+Result<std::string, int> get_cgi_result_via_socket(int socket_fds[2], int pid) {
+	Result<int, int> io_result, wait_result;
+	Result<std::string, int> recv_result;
+	std::string	cgi_result;
+	std::string err_info;
 	bool		is_error = false;
 	bool		is_timeout = false;
-	Result<int, std::string> socketpair_result, fork_result;
-	Result<int, std::string> parent_proc_result;
-	Result<int, int> io_result;
-	int child_status;
-	pid_t wait_result;
-	time_t start_time;
-	int kTIMEOUT_SEC = 2;
+	int			child_status;
 
 	errno = 0;
 	if (close(socket_fds[WRITE]) == CLOSE_ERROR) {
@@ -216,60 +284,32 @@ Result<std::string, int> get_cgi_result_via_socket(int socket_fds[2], int pid) {
 
 	io_result = wait_for_io_ready(socket_fds[READ]);
 	if (io_result.is_err()) {
-		is_error = true;
+		is_error |= true;
 	} else if (io_result.get_ok_value() == IO_TIMEOUT) {
-		is_timeout = true;
+		is_timeout |= true;
 	}
 
-	start_time = time(NULL);
-	cgi_result = "";
-	while (true) {
-		if (is_error || is_timeout) {
-			break;
-		}
-
-		if (is_exec_timeout(start_time, kTIMEOUT_SEC)) {
-			is_timeout = true;
-			break;
-		}
-
-		errno = 0;
-		read_bytes = recv(socket_fds[READ], buf, BUFSIZ - 1, FLAG_NONE);
-		if (read_bytes == -1) {
-			err_info = create_error_info(errno, __FILE__, __LINE__);
-			std::cerr << err_info << std::endl;  // todo: tmp
-			is_error = true;
-			break;
-		}
-		if (read_bytes == 0) {
-			break;
-		}
-		buf[read_bytes] = '\0';
-		cgi_result.append(buf);
+	recv_result = recv_cgi_result(socket_fds[READ], &is_error, &is_timeout);
+	if (recv_result.is_ok()) {
+		cgi_result = recv_result.get_ok_value();
 	}
 
 	errno = 0;
 	if (close(socket_fds[READ]) == CLOSE_ERROR) {
 		err_info = create_error_info(errno, __FILE__, __LINE__);
 		std::cerr << err_info << std::endl;  // todo: tmp
-		is_error = true;
+		is_error |= true;
 	}
 
-	errno = 0;
-	child_status = EXIT_SUCCESS;
-	wait_result = waitpid(pid, &child_status, WNOHANG);
-	// std::cout << CYAN << "errno:" << errno << ", ECHILD:" << ECHILD << RESET << std::endl;
-	if (wait_result == STATUS_SERVER_ERROR && errno != ECHILD) {
-		errno = 0;
-		if (kill(pid, SIGKILL) == STATUS_SERVER_ERROR) {
-			err_info = create_error_info(errno, __FILE__, __LINE__);
-			std::cerr << err_info << std::endl;  // todo: tmp
-			is_error = true;
-		}
+	wait_result = wait_child_process(pid);
+	if (wait_result.is_err()) {
+		is_error |= true;
 	}
+	child_status = wait_result.get_ok_value();
 	if (child_status != EXIT_SUCCESS) {
-		is_error = true;
+		is_error |= true;
 	}
+
 	if (is_timeout) {
 		std::cerr << "[Error] CGI Script Execution Timeout" << std::endl;  // todo: tmp
 		return Result<std::string, int>::err(STATUS_BAD_REQUEST);  // todo: tmp
@@ -290,8 +330,8 @@ void skip_field_lines(std::istringstream *iss) {
 // 一旦、ヘッダーを全て削除し、bodyのみとする
 // field-lineはparseしたり、上流のfield-lineとの結合が必要...
 Result<std::string, int> translate_to_http_protocol(const std::string &cgi_result) {
-	std::string	cgi_body, line;
-	std::istringstream iss(cgi_result);
+	std::string			cgi_body, line;
+	std::istringstream	iss(cgi_result);
 
 	skip_field_lines(&iss);
 
@@ -305,13 +345,11 @@ Result<std::string, int> translate_to_http_protocol(const std::string &cgi_resul
 
 Result<std::string, int> HttpResponse::get_cgi_result(const std::string &file_path,
 													  const std::string &query) const {
-	std::string refactor_content;
+	Result<std::string, int> execute_cgi_result, translate_result;
+	Result<int, std::string> socketpair_result;
 	std::string err_info;
 	int			socket_fds[2];
 	pid_t		pid;
-	Result<int, std::string> socketpair_result;
-	Result<std::string, int> execute_cgi_result, translate_result;
-	std::string body;
 
 	socketpair_result = create_socketpair(socket_fds);
 	if (socketpair_result.is_err()) {
