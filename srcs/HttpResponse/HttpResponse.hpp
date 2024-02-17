c478ecdfce6770d9c5c4f387d90dd09178928af0
takira <ca.fie.0220@gmail.com>
2024-02-17 22:59:25 +0900

    update: http response for outline

--- srcs/HttpResponse/HttpResponse.hpp
+++ srcs/HttpResponse/HttpResponse.hpp
@@ -20,41 +20,6 @@
 # define STATUS_NOT_ACCEPTABLE	406
 # define STATUS_SERVER_ERROR	500
 
-extern const char error_301_page[];
-extern const char error_302_page[];
-extern const char error_303_page[];
-extern const char error_307_page[];
-extern const char error_308_page[];
-extern const char error_400_page[];
-extern const char error_401_page[];
-extern const char error_402_page[];
-extern const char error_403_page[];
-extern const char error_404_page[];
-extern const char error_405_page[];
-extern const char error_406_page[];
-extern const char error_408_page[];
-extern const char error_409_page[];
-extern const char error_410_page[];
-extern const char error_411_page[];
-extern const char error_412_page[];
-extern const char error_413_page[];
-extern const char error_414_page[];
-extern const char error_415_page[];
-extern const char error_416_page[];
-extern const char error_421_page[];
-extern const char error_429_page[];
-extern const char error_494_page[];
-extern const char error_495_page[];
-extern const char error_496_page[];
-extern const char error_497_page[];
-extern const char error_500_page[];
-extern const char error_501_page[];
-extern const char error_502_page[];
-extern const char error_503_page[];
-extern const char error_504_page[];
-extern const char error_505_page[];
-extern const char error_507_page[];
-
 struct file_info {
 	std::string	name;
 	off_t		size;
@@ -122,11 +87,13 @@ class HttpResponse {
 	HttpResponse(const HttpResponse &other);
 	HttpResponse &operator=(const HttpResponse &rhs);
 
-	int get_request_body() { return 200; };
-	int post_request_body() { return 200; }
-	int delete_request_body() { return 200; }
+	int get_request_body(const std::string &target) { (void)target; return STATUS_OK; }
+	int post_request_body(const std::string &target) { (void)target; return STATUS_OK; }
+	int delete_request_body(const std::string &target) { (void)target; return STATUS_OK; }
 
-	std::string get_field_lines() const;
+    static std::string get_resource_path(const std::string &request_target);
+
+    std::string get_field_lines() const;
 
 	// Result<std::string, int> get_path_content(const std::string &path,
 	// 										  bool autoindex,
