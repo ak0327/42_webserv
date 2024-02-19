a92c0629fe7a707a9abebf59b9eb8b65689142a8
takira <ca.fie.0220@gmail.com>
2024-02-19 12:01:57 +0900

    update error_msg and debug func

--- srcs/Error/Error.cpp
+++ srcs/Error/Error.cpp
@@ -23,18 +23,18 @@ std::string get_file_name(const std::string &filepath) {
 
 std::string create_error_info(const std::string &err_str, const std::string &filepath, int line) {
 	std::ostringstream err_info;
-	err_info << err_str << " (" << get_file_name(filepath) << ", L" << line << ")";
+	err_info << err_str << " (" << get_file_name(filepath) << ": L" << line << ")";
 	return err_info.str();
 }
 
 std::string create_error_info(const char *err_str, const std::string &filepath, int line) {
 	std::ostringstream err_info;
-	err_info << err_str << " (" << get_file_name(filepath) << ", L" << line << ")";
+	err_info << err_str << " (" << get_file_name(filepath) << ": L" << line << ")";
 	return err_info.str();
 }
 
 std::string create_error_info(int err_no, const std::string &filepath, int line) {
 	std::ostringstream err_info;
-	err_info << strerror(err_no) << " (" << get_file_name(filepath) << ", L" << line << ")";
+	err_info << strerror(err_no) << " (" << get_file_name(filepath) << ": L" << line << ")";
 	return err_info.str();
 }
