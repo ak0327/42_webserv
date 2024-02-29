ef6af83adc8d4db11a68a36a675c3f73446d4816
takira <ca.fie.0220@gmail.com>
2024-02-29 10:14:54 +0900

    add: test delete

---
+++ test/integration/test_delete.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+source test/integration/test_func.sh
+
+################################################################################
+
+CONF_PATH="test/integration/integration_test.conf"
+TEST_DIR="test/integration/"
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+
+test_cnt=0
+
+ng_cnt=0
+ng_cases=()
+
+skip_cnt=0
+skip_cases=()
+
+################################################################################
+
+./webserv $CONF_PATH &
+
+SERVER_PID=$!
+
+sleep 1
+
+################################################################################
+
+# func            $1:host    $2:port $3:target            $4:check_file_path        $5:expect status          $6:expect_delete
+
+expect_eq_delete "localhost" "4242" "/upload/hello.txt"   "html/upload/hello.txt"   "204 No Content"          true
+expect_eq_delete "localhost" "4242" "/upload/index.html"  "html/upload/index.html"  "204 No Content"          true
+expect_eq_delete "localhost" "4242" "/upload/sub/a.txt"   "html/upload/sub/a.txt"   "204 No Content"          true
+
+expect_eq_delete "localhost" "4242" "/upload/nothing"     ""                        "404 Not Found"           false
+expect_eq_delete "localhost" "4242" "/a/b/c/d/"           ""                        "404 Not Found"           false
+
+expect_eq_delete "localhost" "4242" "/upload/"            "html/upload/"            "404 Not Found"           false # upload/index.html nothing
+
+
+expect_eq_delete "localhost" "4242" "/"                   "html/index.html"         "405 Method Not Allowed"  false
+expect_eq_delete "localhost" "4242" "/../../../"          "html/index.html"         "405 Method Not Allowed"  false
+expect_eq_delete "localhost" "4242" "/index.html"         "html/index.html"         "405 Method Not Allowed"  false
+expect_eq_delete "localhost" "4242" "/404.html"           "html/404.html"           "405 Method Not Allowed"  false
+expect_eq_delete "localhost" "4242" "/cgi-bin/hello.py"   "html/cgi-bin/hello.py"   "405 Method Not Allowed"  false
+expect_eq_delete "localhost" "4242" "/a/b/c/"             "html/a/b/c/file_c.html"  "405 Method Not Allowed"  false
+
+################################################################################
+
+kill $SERVER_PID
+
+################################################################################
+
+echo
+echo "================================================================"
+echo " *** RESULT ***"
+if [ $ng_cnt -eq 0 ] && [ $skip_cnt -eq 0 ]; then
+    echo -e " ${GREEN}All tests passed successfully${RESET}"
+fi
+
+echo "  Total Tests  : $test_cnt"
+
+echo "  Failed Tests : $ng_cnt"
+if [ $ng_cnt -gt 0 ]; then
+    for case in "${ng_cases[@]}"; do
+        echo -e "${RED}     $case${RESET}"
+    done
+fi
+
+echo "  Skipped Tests: $skip_cnt"
+if [ $skip_cnt -gt 0 ]; then
+    for case in "${skip_cases[@]}"; do
+        echo -e "${YELLOW}     $case${RESET}"
+    done
+fi
+
+echo "================================================================"
+
+################################################################################
--- test/integration/test_func.sh
+++ test/integration/test_func.sh
@@ -56,3 +56,74 @@ expect_eq_get() {
         ng_cases+=("No.${test_cnt} (L${call_line}): Request-Body NG: [${response}]")
     fi
 }
+
+
+expect_eq_delete() {
+    local host=$1
+    local port=$2
+    local path=$3
+    local check_file_path=$4
+
+    local expected_status=$5
+    local expected_delete=$6
+
+    local response_file="${TEST_DIR}response.txt"
+
+    local call_line=${BASH_LINENO[0]}
+
+    echo "----------------------------------------------------------------"
+    ((test_cnt++))
+    echo "TEST No.${test_cnt} (L${call_line})"
+
+    if [ -n "$check_file_path" ]; then
+      touch "$check_file_path"
+    fi
+
+    local cmd
+    cmd="-is -X DELETE ${host}:${port}${path}"
+    curl $cmd > "$response_file"
+
+
+    local actual_start_line
+    actual_start_line=$(head -n 1 ${response_file} | tr -d '\r')
+
+    local expected_start_line
+    expected_start_line="HTTP/1.1 ${expected_status}"
+
+    echo -n " Start-Line : "
+    if [[ "$expected_start_line" == "$actual_start_line" ]]; then
+        echo -e "${GREEN}OK${RESET}"
+    else
+        ((ng_cnt++))
+        ng_cases+=("No.${test_cnt} (L${call_line}): Start-Line NG: [${cmd}]")
+        echo -e "${RED}NG -> Expected: \"$expected_start_line\", Actual: \"$actual_start_line\"${RESET}"
+    fi
+
+
+    echo -n " Delete     : "
+    local is_deleted
+    if [[ -z "$check_file_path" ]]; then
+      is_deleted="false"
+    elif [[ -f "$check_file_path" || -d "$check_file_path" ]]; then
+      is_deleted="false"
+    else
+      is_deleted="true"
+    fi
+
+#    echo "path: $check_file_path -> is_deleted: $is_deleted, expected_delete: $expected_delete"
+
+    if [[ "$expected_delete" == "$is_deleted" ]]; then
+        echo -e "${GREEN}OK${RESET}"
+    else
+        ((ng_cnt++))
+        ng_cases+=("No.${test_cnt} (L${call_line}): Delete NG: [${cmd}]")
+        echo -e "${RED}NG${RESET}"
+    fi
+
+
+    if [[ "$expected_delete" == "true" ]] && [ -f "$check_file_path" ]; then
+        [ -f $check_file_path ] && rm "$check_file_path"
+    fi
+
+    [ -f "$response_file" ] && rm "$response_file"
+}
