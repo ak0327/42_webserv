94222b18c3c63d7d0e9e3ac46fa59436fc6df612
takira <ca.fie.0220@gmail.com>
2024-03-10 20:24:54 +0900

    update: local redirect

--- srcs/HttpResponse/GET/get_request_body.cpp
+++ srcs/HttpResponse/GET/get_request_body.cpp
@@ -23,13 +23,12 @@
 
 
 bool HttpResponse::is_cgi_file() const {
-    Result<bool, int> result = Config::is_cgi_mode_on(this->server_config_,
-                                                      this->request_.target());
-    if (result.is_err()) {
+    Result<bool, int> cgi_mode = Config::is_cgi_mode_on(this->server_config_,
+                                                       this->request_.target());
+    if (cgi_mode.is_err()) {
         return false;
     }
-    bool cgi_mode = result.ok_value();
-    if (!cgi_mode) {
+    if (!cgi_mode.ok_value()) {
         return false;
     }
     return Config::is_cgi_extension(this->server_config_,
@@ -45,28 +44,26 @@ bool HttpResponse::is_redirect() const {
 
 
 StatusCode HttpResponse::get_redirect_content(const ReturnDirective &redirect) {
-    DEBUG_PRINT(RED, "redirect.text: %s (L:%d)", redirect.text.c_str(), __LINE__);
     std::string redirect_path;
     if (HttpMessageParser::is_absolute_uri(redirect.text)) {
-        DEBUG_PRINT(RED, "external redirect", __LINE__);
         // external redirect
         redirect_path = redirect.text;
     } else {
-        DEBUG_PRINT(RED, "local redirect", __LINE__);
+        // local redirect
         Result<HostPortPair, StatusCode> info_result = this->request_.server_info();
         if (info_result.is_err()) {
             return info_result.err_value();
         }
-        // local redirect
-        HostPortPair server_info = info_result.ok_value();
-        redirect_path = "http://";
-
-        redirect_path.append(server_info.first);
-        redirect_path.append(":");
-        if (!server_info.second.empty()) {
-            redirect_path.append(server_info.second);
+        HostPortPair host_header = info_result.ok_value();
+        redirect_path = "http://" + host_header.first + ":";
+        if (!host_header.second.empty()) {
+            redirect_path.append(host_header.second);
         } else {
             redirect_path.append(this->server_listen_.first);
+        }
+
+        if (!redirect.text.empty() && redirect.text[0] != '/') {
+            redirect_path.append("/");
         }
         redirect_path.append(redirect.text);
     }
@@ -84,11 +81,6 @@ bool HttpResponse::has_valid_index_page() const {
 
 
 bool HttpResponse::is_method_available() const {
-    // Result<bool, int> allowed;
-    // allowed = Config::is_method_allowed(this->server_config_,
-    //                                     this->request_.target(),
-    //                                     this->request_.method());
-    // return allowed.is_ok() && allowed.ok_value();
     return Config::is_method_allowed(this->server_config_,
                                     this->request_.target(),
                                     this->client_listen_,
@@ -130,7 +122,8 @@ std::string HttpResponse::get_http_date() {
 
 
 std::string HttpResponse::get_http_date(time_t time) {
-    char date[1024];
+    const int kDateBufSize = 1024;
+    char date[kDateBufSize];
     // IMF-fixdate "%a, %d %b %Y %H:%M:%S GMT"
     std::strftime(date, sizeof(date), "%a, %d %b %Y %H:%M:%S GMT", std::gmtime(&time));
     return std::string(date);
@@ -141,7 +134,8 @@ std::string HttpResponse::get_http_date_jst(time_t time) {
     time_t UTC_TO_JST_OFFSET = 9 * 60 * 60;
     time_t jst_time = time + UTC_TO_JST_OFFSET;
 
-    char date[1024];
+    const int kDateBufSize = 1024;
+    char date[kDateBufSize];
     std::strftime(date, sizeof(date), "%a, %d %b %Y %H:%M:%S JST", std::gmtime(&jst_time));
     return std::string(date);
 }
@@ -154,9 +148,9 @@ std::string HttpResponse::get_http_date_jst(time_t time) {
 //   cgi       -> error / response ; already branched out
 //   api       -> error / response
 StatusCode HttpResponse::get_request_body() {
-    DEBUG_PRINT(YELLOW, "  GET 1 taget[%s]", this->request_.target().c_str());
+    DEBUG_PRINT(YELLOW, "[get_request_body] taget[%s]", this->request_.target().c_str());
     if (!is_method_available()) {
-        DEBUG_PRINT(YELLOW, "  GET 2 err: 405");
+        // DEBUG_PRINT(YELLOW, "  GET 2 err: 405");
         return MethodNotAllowed;
     }
 
@@ -164,30 +158,29 @@ StatusCode HttpResponse::get_request_body() {
         // return response_redirect();
         ReturnDirective redirect = Config::get_return(this->server_config_,
                                                       this->request_.target());
-        DEBUG_PRINT(YELLOW, "  GET 3 -> redirect");
+        DEBUG_PRINT(YELLOW, " -> redirect");
         return get_redirect_content(redirect);
     }
 
     // dynamic?
     //  Yes -> dynamic
     if (is_dynamic_endpoint()) {
-        DEBUG_PRINT(YELLOW, "  GET 4 -> dynamic");
+        DEBUG_PRINT(YELLOW, " -> dynamic");
         return response_dynamic();
     }
 
     const std::string rooted_path = get_rooted_path();
-    DEBUG_PRINT(YELLOW, "  GET 5 rooted_path[%s]", rooted_path.c_str());
+    DEBUG_PRINT(YELLOW, "[get_request_body] rooted_path[%s]", rooted_path.c_str());
     Result<bool, StatusCode> is_directory = FileHandler::is_directory(rooted_path);
     if (is_directory.is_err()) {
-        DEBUG_PRINT(YELLOW, "  GET 6 err: directory");
+        DEBUG_PRINT(YELLOW, " -> err: directory");
         return is_directory.err_value();
     }
     if (is_directory.ok_value()) {
-        DEBUG_PRINT(YELLOW, "  GET 7");
         const std::string &directory_path = rooted_path;
         if (!StringHandler::has_trailing_slash(directory_path)) {
             //  No -> 301
-            DEBUG_PRINT(YELLOW, "  GET 8 -> redirect + /");
+            DEBUG_PRINT(YELLOW, "  -> redirect + /");
             const std::string with_trailing_slash =
                     this->request_.target() + "/";
             return redirect_to(with_trailing_slash);
@@ -199,35 +192,33 @@ StatusCode HttpResponse::get_request_body() {
         //  -> 404
         if (!has_valid_index_page()) {
             if (is_autoindex()) {
-                // return response_autoindex();
-                DEBUG_PRINT(YELLOW, "  GET 8 -> autoindex");
+                DEBUG_PRINT(YELLOW, "  -> autoindex");
                 return get_directory_listing(rooted_path, &this->body_buf_);
             }
-            DEBUG_PRINT(YELLOW, "  GET 9 err: not found");
+            DEBUG_PRINT(YELLOW, "  -> err: not found");
             return NotFound;
         }
     }
 
-    DEBUG_PRINT(YELLOW, "  GET 10");
     Result<std::string, StatusCode> indexed = Config::get_indexed_path(this->server_config_,
                                                                        this->request_.target());
     if (indexed.is_err()) {
-        DEBUG_PRINT(YELLOW, "  GET 11 err: indexd path error");
+        DEBUG_PRINT(YELLOW, " -> err: indexd path error");
         return indexed.err_value();
     }
 
     const std::string indexed_path = indexed.ok_value();
-    DEBUG_PRINT(YELLOW, "  GET 12 indexed_path[%s]", indexed_path.c_str());
+    DEBUG_PRINT(YELLOW, "[get_request_body] indexed_path[%s]", indexed_path.c_str());
     Result<bool, StatusCode> is_file = FileHandler::is_file(indexed_path);
     if (is_file.is_err()) {
-        DEBUG_PRINT(YELLOW, "  GET 13 err: is_file.err");
+        DEBUG_PRINT(YELLOW, " -> err: is_file.err");
         return is_file.err_value();
     }
     if (!is_file.ok_value()) {
-        DEBUG_PRINT(YELLOW, "  GET 14 err: not file");
+        DEBUG_PRINT(YELLOW, " -> err: not file");
         return BadRequest;
     }
-    DEBUG_PRINT(YELLOW, "  GET 15 ok -> file");
+    DEBUG_PRINT(YELLOW, " -> ok get_file_content");
     // return response_static(indexed_path);
     return get_file_content(indexed_path, &this->body_buf_);
 }
